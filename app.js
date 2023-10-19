bf85818f19c136c9733f9f89ba33a6516faf954f
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-10-20 02:50:57 +0300

    ...

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookies = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
 
-const { PORT = 3001, BD_LINK } = process.env;
+const { PORT = 3000, BD_LINK } = process.env;
 const app = express();
 
 const routes = require('./routes/index');
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookies());
 // app.use(cors({ origin: 'https://mesto-react-app.nomoreparties.co', credentials: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['https://react-movies-app.nomoredomainsrocks.ru','http://localhost:3000'], credentials: true }));
 app.use(express.json());
 
 mongoose.connect(BD_LINK, { useNewUrlParser: true, useUnifiedTopology: false }).then(() => {
